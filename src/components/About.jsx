9922077d43be7c291c000dd37586d15b9af25970
DuyKhang <khangtdce181439@fpt.edu.vn>
2025-09-19 11:09:40 +0700

    part 1 react router dom

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="d-flex justify-content-center">Đây là trang giới thiệu</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <h1 className="d-flex justify-content-center">
+            Đây là trang liên hệ
+        </h1>
+    </div>
+  )
+}
+
+export default Contact
